370361a7edb82bf50d8dd9d592dfdf9feca35d83
yonatan8070 <45665471+yonatan8070@users.noreply.github.com>
2021-01-09 00:24:44 +0200

    Change fail color to look better

--- app/src/main/java/com/avhar/launchtracker/LaunchAdapter.java
+++ app/src/main/java/com/avhar/launchtracker/LaunchAdapter.java
@@ -2,12 +2,14 @@ package com.avhar.launchtracker;
 
 import android.content.Context;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
