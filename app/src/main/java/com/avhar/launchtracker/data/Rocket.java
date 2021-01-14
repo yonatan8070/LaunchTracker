d1deacb6410aa5b3fdc16150728ff5b84cf0b58f
yonatan8070 <45665471+yonatan8070@users.noreply.github.com>
2021-01-14 22:36:59 +0200

    Finish Rocket.java

--- app/src/main/java/com/avhar/launchtracker/data/Rocket.java
+++ app/src/main/java/com/avhar/launchtracker/data/Rocket.java
@@ -1,7 +1,7 @@
 package com.avhar.launchtracker.data;
 
-// TODO: Finish implementing this class and add it into Launch
 public class Rocket {
+  private String ll2Id;
   private String name; // In text
   private double length; // In meters
   private double diameter; // In meters
@@ -13,5 +13,77 @@ public class Rocket {
   private String image; // In URL
 
   public Rocket() {
+  }
+
+  public String getLl2Id() {
+    return ll2Id;
+  }
+
+  public void setLl2Id(String ll2Id) {
+    this.ll2Id = ll2Id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public double getLength() {
+    return length;
+  }
+
+  public void setLength(double length) {
+    this.length = length;
+  }
+
+  public double getDiameter() {
+    return diameter;
+  }
+
+  public void setDiameter(double diameter) {
+    this.diameter = diameter;
+  }
+
+  public double getMass() {
+    return mass;
+  }
+
+  public void setMass(double mass) {
+    this.mass = mass;
+  }
+
+  public double getLowEarthCapacity() {
+    return lowEarthCapacity;
+  }
+
+  public void setLowEarthCapacity(double lowEarthCapacity) {
+    this.lowEarthCapacity = lowEarthCapacity;
+  }
+
+  public int getStageCount() {
+    return stageCount;
+  }
+
+  public void setStageCount(int stageCount) {
+    this.stageCount = stageCount;
+  }
+
+  public int getLaunchCost() {
+    return launchCost;
+  }
+
+  public void setLaunchCost(int launchCost) {
+    this.launchCost = launchCost;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
   }
 }
